4b529e53be1959186f7a945b071ea5c14b42e82e
Thom Shaw <31721414+thomshaw92@users.noreply.github.com>
2019-11-11 14:18:56 +1000

    Update LASHiS.sh

--- LASHiS.sh
+++ LASHiS.sh
@@ -96,12 +96,12 @@ Required arguments:
 			  
 	anatomical images	Set of multimodal (T1w or gradient echo, followed by T2w FSE/TSE input)
                                 data. Data must be in the format specified by ASHS & ordered as follows:
-                                {time1_T1w} {time1_T2w} \
-                                {time2_T1w} {time2_T2w} \
+                                {time1_T1w} {time1_T2w} /\
+                                {time2_T1w} {time2_T2w} /\
                                            .
                                            .
                                            .
-                                 {timeN_T1w} {timeN_T2w} ...
+                                {timeN_T1w} {timeN_T2w} ...
 				 
 	Optional arguments:
 
