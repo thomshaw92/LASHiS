215c7ca5c733cfe204fd5855f25c7b7a9b5ecfca
thomshaw92 <uqtshaw@CAI-ALMOND.cai.uq.edu.au>
2019-12-20 14:15:41 +1000

    big commit just to get it all online

---
+++ Experiment_Code/R_experiments/LME_experiment/Scripts/reconcileDataADNI.R
@@ -0,0 +1,66 @@
+# library( ADNIMERGE )
+# 
+
+baseDirectory <- 'C:/Users/uqtshaw/Google\ Drive/Publications/LASHiS/NeuroImage/R1_major/LASHiS/Experiment_Code/R_experiments/LME_experiment/'
+dataDirectory <- paste0( baseDirectory, 'Data/' )
+#sandboxDirectory <- paste0( baseDirectory, 'Sandbox/' )
+figuresDirectory <- paste0( baseDirectory, 'Figures/' )
+
+numberOfRegions <- 8
+hippoVolumePipelineNames <- c( 'FsXs', 'ashs_xs', 'FsLong', 'Diet_LASHiS', 'LASHiS')
+
+hippoVolumeCsvs <- list()
+hippoVolumeData <- list()
+cat( "Reading reconciled data.\n" )
+
+for( i in 1:length( hippoVolumePipelineNames ) )
+{
+  hippoVolumeCsvs[[i]] <- paste0( dataDirectory, 'ADNI_', hippoVolumePipelineNames[i], '_reconciled.csv' )
+  hippoVolumeData[[i]] <- read.csv( hippoVolumeCsvs[[i]] )
+}
+
+# find the image IDs that are present in all pipelines
+intersectImageIds <- c()
+for( i in 1:length( hippoVolumeCsvs ) )
+  {
+  hippoVolumeData[[i]] <- read.csv( hippoVolumeCsvs[[i]] )
+  if( i == 1 )
+    {
+    intersectImageIds <- hippoVolumeData[[i]]$IMAGE_ID    
+    }
+  intersectImageIds <- intersect( hippoVolumeData[[i]]$IMAGE_ID, intersectImageIds )
+}
+
+# get rid of observations outside of those consistent image IDs
+for( i in 1:length( hippoVolumeData ) )
+  {
+  hippoVolumeData[[i]] <- hippoVolumeData[[i]][which( hippoVolumeData[[i]]$IMAGE_ID %in% intersectImageIds ), ]
+  hippoVolumeData[[i]]$IMAGE_ID <- factor( hippoVolumeData[[i]]$IMAGE_ID, levels = intersectImageIds )
+  hippoVolumeData[[i]] <- hippoVolumeData[[i]][order( hippoVolumeData[[i]]$IMAGE_ID ), ]
+
+  thicknessColumns <- ( ncol( hippoVolumeData[[i]] ) - numberOfRegions + 1 ):ncol( hippoVolumeData[[i]] )
+  }
+
+cat( "\n\nCreating new .csv files\n" )
+for( i in 1:length( hippoVolumeData ) )
+{
+  demographicsColumns <- (1:8)
+  thicknessColumns <- ( ncol( hippoVolumeData[[i]] ) - numberOfRegions + 1 ):ncol( hippoVolumeData[[i]] )
+  hippoVolumeData[[i]] <- data.frame(
+    hippoVolumeData[[i]][,demographicsColumns],
+    hippoVolumeData[[i]][,thicknessColumns] )
+}
+
+for( i in 1:length( hippoVolumeData ) )
+  {
+  hippoVolumeData[[i]] <- 
+    hippoVolumeData[[i]][order( hippoVolumeData[[i]]$ID, hippoVolumeData[[i]]$VISIT ),]  
+
+  write.csv( hippoVolumeData[[i]], quote = FALSE, row.names = FALSE, 
+             file = paste0( dataDirectory, "/fullyReconciledADNI_", hippoVolumePipelineNames[i], ".csv" ) )  
+  }
+
+
+
+
+
