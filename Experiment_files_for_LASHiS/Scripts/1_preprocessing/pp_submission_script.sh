937855500a1a6be8259565ddc760fb4d00baa2be
Thom Shaw <31721414+thomshaw92@users.noreply.github.com>
2020-05-18 16:19:47 +1000

    Update pp_submission_script.sh

--- Experiment_files_for_LASHiS/Scripts/1_preprocessing/pp_submission_script.sh
+++ Experiment_files_for_LASHiS/Scripts/1_preprocessing/pp_submission_script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-for subjName in `cat /RDS/Q0747/ADNI_BIDS/LASHiS/Experiment_Code/ADNI_code/ADNI_SUBJNAMES.csv` ; do 
-	qsub -v SUBJNAME=$subjName /RDS/Q0747/ADNI_BIDS/LASHiS/Experiment_Code/ADNI_code/1_preprocessing/pp_pbs_script.pbs
+for subjName in `cat ADNI_SUBJNAMES.csv` ; do 
+	qsub -v SUBJNAME=$subjName /path/to/1_preprocessing/pp_pbs_script.pbs
 done
